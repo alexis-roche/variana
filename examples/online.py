27386c5ed2a5b512653b09d326d31a30cc0b4907
Alexis Roche <alexis.roche@gmail.com>
2019-06-06 13:25:58 +0100

    Working version of online VS

--- examples/online.py
+++ examples/online.py
@@ -1,9 +1,25 @@
+"""TODO
+
+Relier cet algorithme à la descente stochastique 2e ordre que nous
+avons implémetentée pour ReidNet (SEP).
+
+Comprendre les conditions de fonctionnement en grande
+dimension. A-t-on besoin de travailler par blocs de paramètres?
+
+A priori sur les paramètres de 2nd ordre de la forme: log pi(theta) =
+lda * theta?
+
+"""
+
 import numpy as np
 from variana.dist_fit import LaplaceApproximation
 from variana.gaussian import FactorGaussian
 
+import pylab as pl
+
 
 SQRT_TWO = np.sqrt(2)
+HUGE_LOG = np.log(np.finfo(float).max)
 
 
 def logZ(logK, v):
@@ -15,10 +31,21 @@ def mahalanobis(x, m, v):
     return np.sum((x - m) ** 2 / v)
 
 
+def diff_exp(x, y, s):
+    """
+    Compute s * (exp(x) - exp(y)) in a wise manner
+    Assume s > 0
+    """
+    m = np.maximum(x, y)
+    d = np.exp(x - m) - np.exp(y - m)
+    return np.exp(np.minimum(m + np.log(s), HUGE_LOG)) * d
+
+
 class OnlineFit(object):
 
     def __init__(self, log_factor, m, v, vmax=1e5):
         self._log_factor = log_factor
+        self._dim = len(m)
         self._logK = log_target(m)
         self._m = np.asarray(m, dtype=float)
         self._v = np.asarray(v, dtype=float)
@@ -26,21 +53,37 @@ class OnlineFit(object):
         self._m_cavity = self._m.copy()
         self._v_cavity = self._v.copy()
         self._vmax = float(vmax)
-        self._dim = len(m)
+        self._f = np.zeros(2 * self._dim + 1)
+
+    def update(self, dtheta, tiny=1e-50):
+        prec_ratio = np.maximum(1 - SQRT_TWO * dtheta[(self._dim + 1):], self._v / self._vmax)
+        self._v /= prec_ratio
+        mean_diff = (self._v / prec_ratio) * dtheta[1:(self._dim + 1)]
+        self._m += mean_diff
+        self._logK += dtheta[0] + .5 * (np.sum(prec_ratio + mean_diff ** 2 / self._v) - dim)
 
     def ortho_basis(self, x):
         phi1 = (x - self._m) / np.sqrt(self._v)
         phi2 = (phi1 ** 2 - 1) / SQRT_TWO
         return np.append(1, np.concatenate((phi1, phi2)))
 
-    def update(self, theta, tiny=1e-50):
-        inv_var_ratio = np.maximum(1 - SQRT_TWO * theta[(self._dim + 1):], tiny)
-        self._v /= inv_var_ratio
-        np.minimum(self._v, self._vmax, out=self._v)
-        mean_diff = (self._v / inv_var_ratio) * theta[1:(self._dim + 1)]
-        self._m += mean_diff
-        self._logK += theta[0] + .5 * (np.sum(inv_var_ratio + mean_diff ** 2 / self._v) - dim)
-
+    """
+    def update(self, dtheta):
+        th0 = self._logK - .5 * np.sum(self._m ** 2 / self._v) + dtheta[0]
+        th1 = self._m / self._v + dtheta[1:(dim + 1)]
+        th2 = np.minimum(-.5 / self._v + dtheta[(self._dim + 1):], -.5 / self._vmax)
+        self._v = - .5 / th2
+        self._m = self._v * th1
+        self._logK = th0 + .5 * np.sum(self._m ** 2 / self._v)
+        
+    def ortho_basis(self, x):
+        xc = (x - self._m) / np.sqrt(self._v)
+        th2 = (xc ** 2 - 1) / (2 * self._v)
+        th1 = xc / np.sqrt(self._v) - 2 * self._m * th2
+        th0 = 1 - np.sum(self._m * th1 + (self._m ** 2 + self._v) * th2)
+        return np.append(th0, np.concatenate((th1, th2)))
+    """
+    
     def sample(self):
         return np.sqrt(self._v_cavity) * np.random.normal(size=self._dim) + self._m_cavity        
 
@@ -50,10 +93,37 @@ class OnlineFit(object):
     def log_fitted_factor(self, x):
         return self.log(x) + .5 * mahalanobis(x, self._m_cavity, self._v_cavity)
 
-    def get_force(self, x):
-        rho = np.exp(logZ(0, self._v_cavity) - logZ(self._logK, self._v))
-        delta = np.exp(self.log_fitted_factor(x)) - np.exp(self._log_factor(x))
-        return rho * delta * self.ortho_basis(x)
+    def rho(self):
+        return np.exp(logZ(0, self._v_cavity) - logZ(self._logK, self._v))    
+
+    def delta(self, x):
+        return diff_exp(self._log_factor(x), self.log_fitted_factor(x), self.rho())
+        
+    def force(self, x):
+        # Get the optimal descent direction in fixed coordinates
+        return self.delta(x) * self.ortho_basis(x)
+
+    def _record(self, *args):
+        if not hasattr(self, '_rec'):
+            self._rec = []
+        self._rec.append(self._logK)
+    
+    def run(self, stepsize, niter, nsubiter, *args):
+        for j in range(niter):
+            print('Iteration: %d' % j)
+            x = self.sample()
+            f = self.force(x)
+            for i in range(1, nsubiter):
+                beta = 1 / (1 + i)
+                f = (1 - beta) * f + beta * self.force(self.sample())
+            self.update(stepsize * f)
+            self._record(*args)
+
+    def factor_fit(self):
+        ### HACK for now
+        from variana.gaussian import FactorGaussian
+        g = FactorGaussian(self._m, self._v, logK=self._logK) / FactorGaussian(self._m_cavity, self._v_cavity, logK=0)
+        return g
         
     @property
     def logK(self):
@@ -75,7 +145,7 @@ class OnlineFit(object):
     def v(self):
         return self._v
 
-
+    
 
 class OnlineStarFit(OnlineFit):
 
@@ -88,84 +158,23 @@ class OnlineStarFit(OnlineFit):
         self._dim = len(m)
         self._rho_base = (1 - self._alpha) ** (-.5 * self._dim)
         self._log_factor = lambda x: alpha * log_target(x)
-
+        
     def sample(self):
         return np.sqrt(self._v / (1 - self._alpha)) * np.random.normal(size=self._dim) + self._m        
     
     def log_fitted_factor(self, x):
         return self._alpha * self.log(x)
 
-    def get_force(self, x):
-        rho = np.exp(-self._alpha * self._logK) * self._rho_base
-        delta = np.exp(self.log_fitted_factor(x)) - np.exp(self._log_factor(x))
-        return rho * delta * self.ortho_basis(x)
+    def rho(self):
+        return np.exp(-self._alpha * self._logK) * self._rho_base
+
+    def _record(self, *args):
+        if not hasattr(self, '_rec'):
+            self._rec = []
+        self._rec.append((error(self._logK, args[0]), error(self._m, args[1]), error(self._v, args[2])))
+    
+    
         
-
-    
-
-class FuckOnlineStarFit(object):
-
-    def __init__(self, log_target, m, v, vmax=1e5, alpha=0.1):
-        self._logK = log_target(m)
-        self._m = np.asarray(m, dtype=float)
-        self._v = np.asarray(v, dtype=float)
-        self._vmax = float(vmax)
-        self._alpha = float(alpha)
-        self._dim = len(m)
-        self._rho_base = (1 - self._alpha) ** (-.5 * self._dim)
-        self._log_factor = lambda x: alpha * log_target(x)
-
-    def ortho_basis(self, x):
-        phi1 = (x - self._m) / np.sqrt(self._v)
-        phi2 = (phi1 ** 2 - 1) / SQRT_TWO
-        return np.append(1, np.concatenate((phi1, phi2)))
-
-    def update(self, theta, tiny=1e-50):
-        inv_var_ratio = np.maximum(1 - SQRT_TWO * theta[(self._dim + 1):], tiny)
-        self._v /= inv_var_ratio
-        np.minimum(self._v, self._vmax, out=self._v)
-        mean_diff = (self._v / inv_var_ratio) * theta[1:(self._dim + 1)]
-        self._m += mean_diff
-        self._logK += theta[0] + .5 * (np.sum(inv_var_ratio + mean_diff ** 2 / self._v) - dim)
-
-    def sample(self):
-        return np.sqrt(self._v / (1 - self._alpha)) * np.random.normal(size=self._dim) + self._m        
-
-    def log(self, x):
-        return self._logK - .5 * np.sum((x - self._m) ** 2 / self._v)
-    
-    def log_fitted_factor(self, x):
-        return self._alpha * self.log(x)
-
-    def get_force(self, x):
-        rho = np.exp(-self._alpha * self._logK) * self._rho_base
-        delta = np.exp(self.log_fitted_factor(x)) - np.exp(self._log_factor(x))
-        return rho * delta * self.ortho_basis(x)
-        
-    @property
-    def logK(self):
-        return self._logK
-
-    @property
-    def K(self):
-        return np.exp(self._logK)
-
-    @property
-    def Z(self):
-        return np.exp(self._logK + .5 * (self._dim * np.log(2 * np.pi) + np.sum(np.log(self._v))))
-
-    @property
-    def m(self):
-        return self._m
-
-    @property
-    def v(self):
-        return self._v
-
-
-
-    
-
 def toy_score(x, m=0, v=1, K=1, power=2, proper=True):
     x = (x - m) / np.sqrt(v)
     return np.log(K) - np.sum(((2 * proper - 1) ** np.arange(len(x))) * np.abs(x) ** power, 0) / power
@@ -176,19 +185,13 @@ def error(x, xt, tiny=1e-10):
     return np.max(np.abs(x - xt))
     
 
-dim = 25
+dim = 50
 power = 2
 
-
-"""
-K = 1
-m = np.zeros(dim)
-v = np.ones(dim)
-"""
 K = np.random.rand()
 m = 5 * (np.random.rand(dim) - .5)
 v = 5 * (np.random.rand(dim) + 1)
-##v = 1
+#K, m, v = 1, 0, 1
 
 # Laplace approximation
 log_target = lambda x: toy_score(x, m, v, K, power)
@@ -196,57 +199,37 @@ l = LaplaceApproximation(log_target, np.zeros(dim))
 ql = l.fit()
 
 # Online parameters
-alpha = 0.1
-beta = 1 / (100 * dim)
-vmax = 1e4
-#niter = 10000
-#nsubiter = 1
-#gamma =  .01
+alpha = 0.1 / dim
+vmax = 1e2
 m0 = np.zeros(dim)
 v0 = np.full(dim, 1)
 
+"""
 tniter = 1000 * dim
 nsubiter = 1
 niter = tniter // nsubiter
-gamma = min(.1 * nsubiter / dim, 1)
+"""
+niter = 1000 * dim
+nsubiter = 1
+stepsize = .1
+
+print('alpha = %f, stepsize = %f' % (alpha, stepsize))
+
 
 """
 q = OnlineFit(log_target, m0, v0, vmax=vmax)
-f = np.zeros(2 * dim + 1)
-rec = []
-for j in range(niter):
-    print('Iteration: %d' % j)
-    # Loop to minimize D(wf||wg)
-    for i in range(nsubiter):
-        x = q.sample()
-        f = (1 - beta) * f + beta * q.get_force(x)
-    rec.append(q.logK)
-    #rec.append((error(q.K, K), error(q.m, m), error(q.v, v)))
-    q.update(-gamma * f)
-rec = np.array(rec)
-
-### HACK for now
-from variana.gaussian import FactorGaussian
-g = FactorGaussian(q.m, q.v, logK=q.logK) / FactorGaussian(m0, v0, logK=0)
+q.run(stepsize, niter, nsubiter)
+g = q.factor_fit()
+pl.figure()
+pl.plot(q._rec)
+pl.show()
 """
-
-q = OnlineStarFit(log_target, m0, v0, vmax=vmax)
-f = np.zeros(2 * dim + 1)
-rec = []
-for j in range(niter):
-    print('Iteration: %d' % j)
-    # Loop to minimize D(wf||wg)
-    for i in range(nsubiter):
-        x = q.sample()
-        f = (1 - beta) * f + beta * q.get_force(x)
-    #rec.append(q.logK)
-    rec.append((error(q.K, K), error(q.m, m), error(q.v, v)))
-    q.update(-gamma * f)
-rec = np.array(rec)
-
-import pylab as pl
-pl.plot(rec)
+q = OnlineStarFit(log_target, m0, v0, vmax=vmax, alpha=alpha)
+q.run(stepsize, niter, nsubiter, np.log(K), m, v)
+pl.figure()
+pl.plot(q._rec)
 pl.legend(('K', 'm', 'v'))
 pl.show()
+
 
 
