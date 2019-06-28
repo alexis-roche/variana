ed50e038120c353149516aad6375918eee35c429
Alexis Roche <alexis.roche@gmail.com>
2019-06-28 17:29:53 +0100

    Attempt at online inclusive KL minimization

--- examples/online.py
+++ examples/online.py
@@ -41,6 +41,16 @@ def mahalanobis(x, m, v):
     return np.sum((x - m) ** 2 / v)
 
 
+def split_max(a, b):
+    """
+    Compute m = max(a, b) and returns a tuple (m, a - m, b - m)
+    """
+    if a > b:
+        return a, 0.0, b - a
+    else:
+        return b, a - b, 0.0
+
+
 def safe_exp(x, log_s):
     """
     Compute s * exp(x)
@@ -52,25 +62,38 @@ def safe_diff_exp(x, y, log_s):
     """
     Compute s * (exp(x) - exp(y))
     """
+    """
     m = np.maximum(x, y)
     d = np.exp(x - m) - np.exp(y - m)
+    """
+    m, xc, yc = split_max(x, y)
+    d = np.exp(xc) - np.exp(yc)
     return safe_exp(m, log_s) * d
 
 
 
 class OnlineFit(object):
 
-    def __init__(self, log_target, dim, gamma, vmax=1e5):
-        self._gen_init(log_target, dim, vmax)
+    def __init__(self, log_target, dim, gamma, vmax=1e5, logK=None, m=None, v=None):
+        self._gen_init(log_target, dim, vmax, logK, m, v)
         self.reset(gamma)
 
-    def _gen_init(self, log_target, dim, vmax):
+    def _gen_init(self, log_target, dim, vmax, logK, m, v):
         self._dim = dim
         self._vmax = float(vmax)
         self._log_target = log_target
-        self._logK = log_target(m)
-        self._m = np.zeros(dim)
-        self._v = np.ones(dim)
+        if m is None:
+            self._m = np.zeros(dim)
+        else:
+            self._m = np.asarray(m)
+        if v is None:
+            self._v = np.ones(dim)
+        else:
+            self._v = np.asarray(v)
+        if logK is None:
+            self._logK = log_target(self._m)
+        else:
+            self._logK = float(logK)        
         self._force = self.force
         # Init ground truth parameters
         self._logZt = 0
@@ -173,10 +196,10 @@ class OnlineFit(object):
 
 class OnlineContextFit(OnlineFit):
 
-    def __init__(self, log_factor, m, v, gamma, vmax=1e5, proxy='discrete_kl'):
-        self._gen_init(log_factor, len(m), vmax)
-        self._m_cavity = np.asarray(m, dtype=float)
-        self._v_cavity = np.asarray(v, dtype=float)
+    def __init__(self, log_factor, m, v, gamma, vmax=1e5, proxy='discrete_kl', logK=None):
+        self._gen_init(log_factor, len(m), vmax, logK, m, v)
+        self._m_cavity = np.asarray(m, dtype=float).copy()
+        self._v_cavity = np.asarray(v, dtype=float).copy()
         self._log_factor = log_factor
         self._log_target = None
         if proxy == 'likelihood':
@@ -209,11 +232,12 @@ class OnlineContextFit(OnlineFit):
     def stepsisze(self):
         return self._gamma
 
-        
+
+    
 class OnlineStarFit(OnlineFit):
 
-    def __init__(self, log_target, dim, alpha, gamma, vmax=1e5, proxy='discrete_kl'):
-        self._gen_init(log_target, dim, vmax)
+    def __init__(self, log_target, dim, alpha, gamma, vmax=1e5, proxy='discrete_kl', logK=None, m=None, v=None):
+        self._gen_init(log_target, dim, vmax, logK, m, v)
         if proxy == 'likelihood':
             self._force = self.force_likelihood
         elif proxy != 'discrete_kl':
@@ -243,12 +267,81 @@ class OnlineStarFit(OnlineFit):
     def stepsize(self):
         return self._gamma * (1 - self._alpha) ** (self._dim / 2)
 
+
+    
+class OnlineKLFit(OnlineFit):
+
+    def __init__(self, log_target, dim, gamma, lda=1, vmax=1e5, proxy='discrete_kl', logK=None, m=None, v=None):
+        self._gen_init(log_target, dim, vmax, logK, m, v)
+        if proxy == 'likelihood':
+            self._force = self.force_likelihood
+        elif proxy != 'discrete_kl':
+            raise ValueError('Unknown proxy')
+        self.reset(lda, gamma)
+        
+    def reset(self, lda, gamma=None):
+        self._lda = float(lda)
+        if not gamma is None:
+            self._gamma = float(gamma)
+        
+    def sample(self):
+        return np.sqrt(self._v / self._lda) * np.random.normal(size=self._dim) + self._m
+    
+    def epsilon(self, x):
+        log_p = self._log_target(x)
+        log_q = self.log(x)
+        zob1 = -self._lda * log_q
+        zob2 = log_q + zob1
+        return safe_diff_exp(log_p + zob1, zob2, 0)
+    
+    def force_likelihood(self, x):
+        raise NotImplementedError('This is a test piece of code.')
+
+
+
+class OnlineKLFit0(OnlineFit):
+
+    def __init__(self, log_target, dim, gamma, lda=0, vmax=1e5, proxy='discrete_kl', logK=None, m=None, v=None):
+        self._gen_init(log_target, dim, vmax, logK, m, v)
+        if proxy == 'likelihood':
+            self._force = self.force_likelihood
+        elif proxy != 'discrete_kl':
+            raise ValueError('Unknown proxy')
+        self.reset(lda, gamma)
+        
+    def log_pi(self, x):
+        return -.5 * (self._dim * np.log(2 * np.pi * self._vmax) + mahalanobis(x, 0, self._vmax))
+
+    def reset(self, lda, gamma=None):
+        self._lda = float(lda)
+        if not gamma is None:
+            self._gamma = float(gamma)
+        
+    def sample(self):
+        if np.random.rand() > (1 - self._lda):
+            return np.sqrt(self._vmax) * np.random.normal(size=self._dim)
+        return np.sqrt(self._v) * np.random.normal(size=self._dim) + self._m
+    
+    def epsilon(self, x):
+        log_p = self._log_target(x)
+        log_q = self.log(x)
+        log_q0 = self.logZ + self.log_pi(x)
+        max1, log_pc, log_qc = split_max(log_p, log_q)
+        num = np.exp(log_pc) - np.exp(log_qc)
+        max2, log_qc, log_q0c = split_max(log_q, log_q0)
+        den = (1 - self._lda) * np.exp(log_qc) + self._lda * np.exp(log_q0c)
+        return np.exp(max1 - max2) * (num / den)
+
+    def force_likelihood(self, x):
+        raise NotImplentedError('This is a test piece of code.')
+
     
 
 class OnlineStarTaylorFit(OnlineFit):
 
-    def __init__(self, log_target, dim, alpha, vmax=1e5, epsilon=1e-5, grad=None, hess_diag=None):
-        self._gen_init(log_target, dim, vmax)
+    def __init__(self, log_target, dim, alpha, vmax=1e5, epsilon=1e-5, grad=None, hess_diag=None,
+                 logK=None, m=None, v=None):
+        self._gen_init(log_target, dim, vmax, logK, m, v)
         self._epsilon = float(epsilon)
         self.reset(alpha, grad=grad, hess_diag=hess_diag)
 
@@ -285,7 +378,8 @@ def error(x, xt, tiny=1e-10):
    
 
 dim = 10
-beta = 1.5
+beta = 1
+vmax = 1e2
 K = np.random.rand()
 m = 5 * (np.random.rand(dim) - .5)
 s2 = 5 * (np.random.rand(dim) + 1)
@@ -301,7 +395,6 @@ ql = l.fit()
 # Online parameters
 proxy = 'discrete_kl'
 ###proxy = 'likelihood'
-vmax = 1e2
 gamma_s = .01 / np.sqrt(dim)
 ###alpha = .1 / np.sqrt(dim)
 ###gamma = gamma_s / alpha
@@ -324,18 +417,16 @@ q.run(niter, record=True)
 print('Error = %3.2f %3.2f %3.2f' % q.error())
 q.disp('simple')
 
+"""
 print('Star fit: alpha = %f, gamma = %f' % (alpha, gamma))
 qs = OnlineStarFit(target.log, dim, alpha, gamma, vmax=vmax, proxy=proxy)
 qs.ground_truth(target.logZ, target.m, target.v)
 qs.run(niter, record=True)
-"""
-qs.reset(alpha=.5)
-print('Star fit: alpha = %f, gamma = %f' % (qs._alpha, qs._gamma))
-qs.run(niter, record=True)
-"""
+###qs.reset(alpha=.5)
+###print('Star fit: alpha = %f, gamma = %f' % (qs._alpha, qs._gamma))
+###qs.run(niter, record=True)
 print('Error = %3.2f %3.2f %3.2f' % qs.error())
 qs.disp('star')
-
 
 print('Star-Taylor fit: alpha = %f' % alpha)
 qst = OnlineStarTaylorFit(target.log, dim, alpha, vmax=vmax, grad=target.grad_log, hess_diag=target.hess_diag_log)
@@ -343,4 +434,32 @@ qst.ground_truth(target.logZ, target.m, target.v)
 qst.run(niter, record=True)
 print('Error = %3.2f %3.2f %3.2f' % qst.error())
 qst.disp('star-taylor')
+"""
 
+"""
+gamma1 = 1e-3 / dim
+log_target = lambda x: target.log(x) - .5 * np.sum(x ** 2) / vmax
+q1 = OnlineKLFit(log_target, dim, gamma1, lda=.5, logK=q.logK, m=q.m, v=q.v)
+"""
+
+q1 = OnlineKLFit(target.log, dim, gamma_s, lda=.75, logK=q.logK, m=q.m, v=q.v)
+q1.ground_truth(target.logZ, target.m, target.v)
+q1.run(10 * niter, nsubiter=1, record=True)
+print('Error = %3.2f %3.2f %3.2f' % q1.error())
+q1.disp('online KL')
+###pl.plot(pl.axis()[-1] * np.array(q1._zob) / np.max(q1._zob), ':')
+
+
+r"""
+q = FactorGaussian(target.m, target.v, logZ=target.logZ)
+N = 100000
+X = q.random(N)
+log_p = np.array([target.log(x) for x in X.T])
+log_q = q.log(X)
+log_r = log_p - log_q
+z = np.exp(log_r) - 1
+
+pl.figure()
+pl.hist(z)
+pl.show()
+"""
